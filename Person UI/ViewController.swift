8b5491c37e4c9831b5eca84aaee403acd7c8c9b3
Eric <emitchell008@gmail.com>
2019-02-15 15:50:49 -0600

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Eric Mitchell on 2/15/19.
+//  Copyright © 2019 Eric Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
